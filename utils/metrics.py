e94dd9c8e83e5b7ed93ad7d1c2614ac152c7fd68
SphericalCow <acciorocketships@gmail.com>
2018-02-17 23:17:05 -0500

    added softmax

---
+++ utils/metrics.py
@@ -0,0 +1,69 @@
+import keras.backend as K
+import tensorflow as tf
+from keras.objectives import *
+from keras.metrics import binary_crossentropy
+from tensorflow.contrib.metrics import streaming_mean_iou
+import numpy as np
+
+
+def sparse_accuracy_ignoring_last_label(y_true, y_pred):
+    nb_classes = K.int_shape(y_pred)[-1]
+    y_pred = K.reshape(y_pred, (-1, nb_classes))
+
+    y_true = K.one_hot(tf.to_int32(K.flatten(y_true)),
+                       nb_classes + 1)
+    unpacked = tf.unstack(y_true, axis=-1)
+    legal_labels = ~tf.cast(unpacked[-1], tf.bool)
+    y_true = tf.stack(unpacked[:-1], axis=-1)
+
+    return K.sum(tf.to_float(legal_labels & K.equal(K.argmax(y_true, axis=-1), K.argmax(y_pred, axis=-1)))) / K.sum(tf.to_float(legal_labels))
+
+
+# Softmax cross-entropy loss function for pascal voc segmentation
+# and models which do not perform softmax.
+# tensorflow only
+def softmax_sparse_crossentropy_ignoring_last_label(y_true, y_pred):
+    y_pred = K.reshape(y_pred, (-1, K.int_shape(y_pred)[-1]))
+    log_softmax = tf.nn.log_softmax(y_pred)
+
+    y_true = K.one_hot(tf.to_int32(K.flatten(y_true)), K.int_shape(y_pred)[-1]+1)
+    unpacked = tf.unstack(y_true, axis=-1)
+    y_true = tf.stack(unpacked[:-1], axis=-1)
+
+    cross_entropy = -K.sum(y_true * log_softmax, axis=1)
+    cross_entropy_mean = K.mean(cross_entropy)
+
+    return cross_entropy_mean
+
+
+# Softmax cross-entropy loss function for coco segmentation
+# and models which expect but do not apply sigmoid on each entry
+# tensorlow only
+def binary_crossentropy_with_logits(ground_truth, predictions):
+    return K.mean(K.binary_crossentropy(ground_truth,
+                                        predictions,
+                                        from_logits=True),
+                                        axis=-1)
+
+# X: ndarray
+# axis: axis to compute values along. Default is the first non-singleton axis.
+def softmax(X, axis=None):
+    theta = 1.0
+    # make X at least 2d
+    y = np.atleast_2d(X)
+    # find axis
+    if axis is None:
+        axis = next(j[0] for j in enumerate(y.shape) if j[1] > 1)
+    # multiply y against the theta parameter, 
+    y = y * float(theta)
+    # subtract the max for numerical stability
+    y = y - np.expand_dims(np.max(y, axis = axis), axis)
+    # exponentiate y
+    y = np.exp(y)
+    # take the sum along the specified axis
+    ax_sum = np.expand_dims(np.sum(y, axis = axis), axis)
+    # finally: divide elementwise
+    p = y / ax_sum
+    # flatten if X was 1D
+    if len(X.shape) == 1: p = p.flatten()
+    return p
